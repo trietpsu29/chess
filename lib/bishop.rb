4bb85ccf604e16283ceb54e65e2150434e551585
trietpsu29 <112594221+trietpsu29@users.noreply.github.com>
2026-07-25 11:09:20 +0700

    Implement check_piece_move

--- lib/bishop.rb
+++ lib/bishop.rb
@@ -6,4 +6,14 @@ class Bishop < Piece
     super
     @symbol = color == :white ? '♗' : '♝'
   end
+
+  def check_piece_move(start, des, _des_color)
+    start_row = start[1].to_i
+    des_row = des[1].to_i
+    start_col = start[0].ord
+    des_col = des[0].ord
+    diff_row = (start_row - des_row).abs
+    diff_col = (start_col - des_col).abs
+    diff_row == diff_col
+  end
 end
---
+++ spec/bishop_spec.rb
@@ -0,0 +1,27 @@
+require_relative '../lib/bishop'
+
+describe Bishop do
+  subject(:bishop) { described_class.new(:white) }
+
+  describe '#check_piece_move' do
+    it 'returns true when moving diagonally up-right' do
+      expect(bishop.check_piece_move('c3', 'f6', nil)).to be true
+    end
+
+    it 'returns true when moving diagonally down-right' do
+      expect(bishop.check_piece_move('c6', 'f3', nil)).to be true
+    end
+
+    it 'returns true when moving diagonally up-left' do
+      expect(bishop.check_piece_move('f3', 'c6', nil)).to be true
+    end
+
+    it 'returns true when moving diagonally down-left' do
+      expect(bishop.check_piece_move('f6', 'c3', nil)).to be true
+    end
+
+    it 'returns false when not moving diagonally' do
+      expect(bishop.check_piece_move('c3', 'c6', nil)).to be false
+    end
+  end
+end
