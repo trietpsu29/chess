9a8a93efda7b9d1c9d6bfd22c6eafc507c04031d
trietpsu29 <112594221+trietpsu29@users.noreply.github.com>
2026-07-25 10:49:46 +0700

    Fix check_block_move method bugs

--- lib/board.rb
+++ lib/board.rb
@@ -15,13 +15,12 @@ class Board
   end
 
   def check_block_move?(start, des)
-    start_color = @grid[start].color
     start_col = start[0].ord
     des_col = des[0].ord
     start_row = start[1].to_i
     des_row = des[1].to_i
 
-    factor_col = start_color == :white ? 1 : -1
+    factor_col = start_row < des_row ? 1 : -1
     factor_row = start_col < des_col ? 1 : -1
     factor_dig = start_col != des_col && start_row != des_row ? true : false
     curr_col = start_col
@@ -29,8 +28,8 @@ class Board
     loop do
       curr_row += factor_col if start_col == des_col || factor_dig
       curr_col += factor_row if start_row == des_row || factor_dig
-      curr = @grid["#{curr_col.chr}#{curr_row.to_i}"]
-      break if curr == @grid[des]
+      curr = @grid["#{curr_col.chr}#{curr_row}"]
+      break if curr_col == des_col && curr_row == des_row
       return true unless curr.nil?
     end
     false
@@ -46,12 +45,14 @@ class Board
     des_color = des_v.nil? ? nil : des_v.color
     return false if start_v.color == des_color
     return false unless start_v.check_piece_move?(start, des, des_color)
-    return false if !start_v.is_a?(Knight) && check_block_move?
+    return false if !start_v.is_a?(Knight) && check_block_move?(start, des)
 
     true
   end
 
   def move_piece(start, des)
+    return unless check_valid_move?
+
     @grid[des] = @grid[start]
     @grid[start] = nil
   end
